25bfe6217f1f28e0d471c63939a1c138e4ab9cd6
Shadi Boomi <shadi.boomi@gmail.com>
2019-12-03 09:10:18 +0100

    Changed application.properties and set hibernate's ddl-auto from 'none' to 'update'

--- src/main/java/com/buzzle/springboot/SpringbootApplication.java
+++ src/main/java/com/buzzle/springboot/SpringbootApplication.java
@@ -1,13 +1,31 @@
 package com.buzzle.springboot;
 
+import com.buzzle.springboot.entity.User;
+import com.buzzle.springboot.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class SpringbootApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SpringbootApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(UserRepository userRepository) {
+        return args -> {
+            Stream.of("janedoes", "anonymouse", "billgates", "elsascupcakes", "thatonefurry").forEach(username -> {
+                User user = new User(username, username+"06" , username.toLowerCase() + "@domain.com");
+                userRepository.save(user);
+            });
+            userRepository.findAll().forEach(System.out::println);
+        };
     }
 
 }
