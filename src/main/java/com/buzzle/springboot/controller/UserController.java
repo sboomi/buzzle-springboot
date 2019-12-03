928746e4e0217e7e321bc3f01ffb9e874000497b
Shadi Boomi <shadi.boomi@gmail.com>
2019-12-03 08:47:57 +0100

    Created a CrossOrigin with Angular

--- src/main/java/com/buzzle/springboot/controller/UserController.java
+++ src/main/java/com/buzzle/springboot/controller/UserController.java
@@ -5,24 +5,22 @@ import com.buzzle.springboot.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
     @Autowired
     private UserServiceImpl userService;
 
     @GetMapping("/showUsers")
-    public String findUsers(Model model){
-        var users = (List<User>) userService.findAll();
-
-            model.addAttribute("users", users);
-
-            return "showUsers";
-
+    public List<User> getUsers() {
+        return (List<User>) userService.findAll();
     }
 
 }
