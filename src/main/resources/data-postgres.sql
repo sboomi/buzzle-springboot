10b7aabf1560b776cdd9f06a54db11109c2a6131
Shadi Boomi <shadi.boomi@gmail.com>
2019-12-03 09:48:49 +0100

    Added data and schema for PostgreSQL

---
+++ src/main/resources/data-postgres.sql
@@ -0,0 +1,8 @@
+INSERT INTO users(username, password, mail) VALUES('janedoes', 'janedoes06', 'janedoe@example.com');
+INSERT INTO users(username, password, mail) VALUES('janedoes', 'janedoes06', 'janedoe@example.com');
+INSERT INTO users(username, password, mail) VALUES('janedoes', 'janedoes06', 'janedoe@example.com');
+INSERT INTO users(username, password, mail) VALUES('janedoes', 'janedoes06', 'janedoe@example.com');
+INSERT INTO users(username, password, mail) VALUES('janedoes', 'janedoes06', 'janedoe@example.com');
+INSERT INTO users(username, password, mail) VALUES('janedoes', 'janedoes06', 'janedoe@example.com');
+INSERT INTO users(username, password, mail) VALUES('janedoes', 'janedoes06', 'janedoe@example.com');
+INSERT INTO users(username, password, mail) VALUES('janedoes', 'janedoes06', 'janedoe@example.com');
