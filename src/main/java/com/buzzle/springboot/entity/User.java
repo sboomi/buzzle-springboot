96c6d4c36a6cabf4c9ab588dfb2ec7a465631c82
Shadi Boomi <shadi.boomi@gmail.com>
2019-12-03 09:25:59 +0100

    Deleted application.properties lines

--- src/main/java/com/buzzle/springboot/entity/User.java
+++ src/main/java/com/buzzle/springboot/entity/User.java
@@ -51,4 +51,15 @@ public class User {
     public void setMail(String mail) {
         this.mail = mail;
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                ", mail='" + mail + '\'' +
+                ", blogs=" + blogs +
+                '}';
+    }
 }
